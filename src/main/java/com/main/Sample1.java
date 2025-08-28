a07dd8195b8d2e0481efc95cbd56b833641f9ba6
SarojSseleniumTester <sarojkdash7@gmail.com>
2025-08-28 16:53:47 +0530

    commiyMessage1

---
+++ src/main/java/com/main/Sample1.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.testng.annotations.Test;
+
+public class Sample1 
+{
+	@Test
+	public void Sample1()
+	{
+		System.out.println("it is sample1");
+	}
+
+}
---
+++ src/test/java/com/test/Sample2.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class Sample2 
+{
+	@Test
+	public void Sample2()
+	{
+		System.out.println("it is sample2");
+	}
+
+}
